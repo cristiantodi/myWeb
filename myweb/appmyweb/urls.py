4439d5d924c575f81c1930e15573b5e3ec3cde55
cristiantodi <toleko89@hotmail.com>
2022-06-01 15:13:53 -0500

    continuacion de app servicios

--- myweb/appmyweb/urls.py
+++ myweb/appmyweb/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from appmyweb import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name="Home"),
     path('tienda', views.tienda, name="Tienda"),
     path('servicios', views.servicios, name="Servicios"),
     path('contacto', views.contacto, name="Contacto"),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
