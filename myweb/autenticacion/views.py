122bc922b906d86b09704537e8e098452127c33d
cristiantodi <toleko89@hotmail.com>
2022-07-13 14:53:13 -0500

    Terminando login - Falta CSS

--- myweb/autenticacion/views.py
+++ myweb/autenticacion/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from requests import request
 # Create your views here.
 
 class Vregistro(View):
-    def get(self, request):
-        form= UserCreationForm()
-        return render(request, "registro/registro.html", {"form":form})
-    
-    def post(self, request):
+    def get (self,request):
+        form = UserCreationForm()
+        return render(request,"registro/registro.html",{"form":form})
         pass
+
+    def post(self, request):
+        form=UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            usuario=form.save()
+            login(request, usuario) #Para realizar login
+            return redirect('Home')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+
+            return render(request,"registro/registro.html",{"form":form})
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('Home')
+
+def Logear(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            nombreUsuario=form.cleaned_data.get("username")
+            contra=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombreUsuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('Home')
+            else:
+                messages.error(request, "Usuario no valido")
+        else:
+            messages.error(request, "Informacion incorrecta")
+    
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form":form})
--- myweb/carro/context_processors.py
+++ myweb/carro/context_processors.py
@@ -1,5 +1,5 @@
 def importe_total_carro (request):
     total=0
-    for key, value in request.session["carro"].items():
-        total=total + float(value["precio"])
+    '''for key, value in request.session["carro"].items():
+        total=total + float(value["precio"])'''
     return{"importe_total_carro":total}
