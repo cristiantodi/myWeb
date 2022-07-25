132ad8bb9beab7e4b9d131a237bd177e65e695ef
cristiantodi <toleko89@hotmail.com>
2022-07-25 10:43:31 -0500

    Finalizado, mirar detalles

--- myWeb/urls.py
+++ myWeb/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path('carro/', include('carro.urls')),
     path('autenticacion/', include('autenticacion.urls')),
     path('pedidos/',include('pedidos.urls')),
-    path('carro/', include('carro.urls')),
 
 ]
